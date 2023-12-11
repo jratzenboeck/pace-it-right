d42606ea187228f6a45090afc2a4817b017f1840
Juergen Ratzenboeck <juergen.ratzenboeck@avocodo.com>
2023-12-11 18:23:35 +0100

    Improve design a little

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,11 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addNunjucksShortcode('resultSectionToggle', function (title) {
 		return `<div class="flex justify-between items-center border p-2 hover:cursor-pointer" @click="sectionVisible = !sectionVisible">
-                <span>${title}</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4" x-show="!sectionVisible" x-transition.opacity.duration.500ms>
+                <div>${title}</div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4" x-show="!sectionVisible" x-transition:enter.duration.500ms>
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4" x-show="sectionVisible" x-transition.opacity.duration.500ms>>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4" x-show="sectionVisible" x-transition:enter.duration.500ms>
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"/>
                 </svg>
             </div>`
